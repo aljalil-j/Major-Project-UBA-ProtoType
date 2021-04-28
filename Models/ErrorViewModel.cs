d9372e53cf2e956644966070200e969d19aaa2b1
Knight_J <55115377+aljalil-j@users.noreply.github.com>
2021-04-28 08:47:04 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Major_Project_UBA_ProtoType.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
