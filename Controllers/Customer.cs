fa4c57958be906cb31ac091594d94cd9c87ee10d
Knight_J <55115377+aljalil-j@users.noreply.github.com>
2021-04-28 10:28:00 -0500

    new

---
+++ Controllers/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Major_Project_UBA_ProtoType.Controllers
+{
+    public class Customer
+    {
+        public string UserName { get; internal set; }
+        public string Id { get; internal set; }
+    }
+}
---
+++ Controllers/DeleteRoleViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Major_Project_UBA_ProtoType.Controllers
+{
+    public class DeleteRoleViewModel
+    {
+        public string Id { get; internal set; }
+    }
+}
